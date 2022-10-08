298a6eef2123b4940b72dbfec30c8967b4d19317
Emma <89472272+emma-devel@users.noreply.github.com>
2022-10-08 20:35:57 +0000

    Add feature to give google maps link

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+import math
 
 regex = r"(?<=99)(?P<LAT>\d\d\d)(?: ?(?P<QUADRANT>\d))(?:\d(?P<LON>\d\d\d))"
 fm13 = input("INPUT FM-13: ")
@@ -29,3 +30,25 @@ for matchNum, match in enumerate(matches, start=1):
         exit()
     print("LAT", LAT, quadLat)
     print("LON", LON, quadLon)
+
+    latDeg = math.floor(float(LAT))
+    latMin = float(float('0.' + LAT.split('.')[1]) * 60)
+    if(latMin.is_integer() == False):
+        latSec = float(float('0.' + str(latMin).split('.')[1]) * 60)
+    else:
+        latSec = 00.0
+    lonDeg = math.floor(float(LON))
+    lonMin = float(float('0.' + LON.split('.')[1]) * 60)
+    if(lonMin.is_integer() == False):
+        lonSec = float(float('0.' + str(lonMin).split('.')[1]) * 60)
+    else:
+        lonSec = 00.0
+    if(latMin.is_integer() == True):
+        latMin = int(latMin)
+    if(lonMin.is_integer() == True):
+        lonMin = int(lonMin)
+    url = "https://www.google.com/maps/place/" + str(latDeg) + '°' + str(latMin) + "'" + str(latSec) + '"' + str(quadLat) + '+' + str(lonDeg) + '°' + str(lonMin) + "'" + str(lonSec) + '"' + str(quadLon)
+    print("Google maps URL: " + url)
+# 43 Degrees
+# 180 minutes
+# 0 Seconds
